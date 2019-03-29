ee8c68b74710f6775b819d9530326f93ad2f32ef
Oliver Martin-Hirsch <oliver.martinhirsch@sky.uk>
2019-03-29 13:11:28 +0000

    fixed bug

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,6 @@
-import { React, Component } from 'react';
+import React, { Component } from 'react';
 
 class QuizQuestion extends Component {
-
     render() {
         return (
             <main>
